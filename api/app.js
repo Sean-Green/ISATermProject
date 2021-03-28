f0c0ee4cf44dba8c3cba3be6282d78caa0f907c2
Sean <sean.joseph.green@gmail.com>
2021-03-28 16:55:26 -0700

    updated the schema again, got rid of player table for now

--- api/app.js
+++ api/app.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  database : 'arcadedb',
+  user     : 'admin',
+  password : 'admin'
+});
+ 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
+app.get('/scores', (req, res) => {
+  console.log(`api key = ${req.query.api}\n url = ${req.hostname}`);
+  connection.query(`SELECT * FROM scores WHERE apiKey = '${req.query.api}'`, function (error, results, fields) {
+    if (error) throw error;
+    // connected!
+    res.send({scores : results});
+  });
 });
 
 app.listen(port, () => {
--- api/schema.sql
+++ api/schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
    email       VARCHAR(32)    NOT NULL, 
    password    VARCHAR(32)    NOT NULL,
    PRIMARY KEY (userID)
-)
+);
 
 -- Create DB
 CREATE TABLE apiKeys(
@@ -12,33 +12,24 @@ CREATE TABLE apiKeys(
    domain      VARCHAR(512)   NOT NULL,
    PRIMARY KEY (apiKey),
    CONSTRAINT fk_userID FOREIGN KEY (userID) REFERENCES users(userID)
-)
-
-CREATE TABLE players(
-   playerID    INT            NOT NULL AUTO_INCREMENT,
-   playerName  VARCHAR(32)    NOT NULL,
-   PRIMARY KEY (playerID)
-); 
+);
 
 CREATE TABLE scores(
-   date        DATETIME       NOT NULL,  -- YYYY-MM-DD HH:MI:SS PM
-   playerID    INT            NOT NULL,
+   scoreDate        DATETIME       NOT NULL,  -- YYYY-MM-DD HH:MI:SS PM
+   playerName  VARCHAR(32)    NOT NULL,
    apiKey      CHAR(16)       NOT NULL,
    score       FLOAT          NOT NULL,
-   PRIMARY KEY (date, playerID),
-   CONSTRAINT fk_apiKey FOREIGN KEY (apiKey) REFERENCES apiKeys(apiKey),
-   CONSTRAINT fk_playerID FOREIGN KEY (playerID) REFERENCES players(playerID)
+   PRIMARY KEY (scoreDate, playerName, apiKey),
+   CONSTRAINT fk_apiKey FOREIGN KEY (apiKey) REFERENCES apiKeys(apiKey)
 );
 
 INSERT INTO users(email, password)
 VALUES ("johnny@scottmail.ca", "ilovescatman");
 
 INSERT INTO apiKeys(userID, apiKey, domain)
-VALUES (1, "0123456789abcdef", "https://www.sean-green-cst.com/");
+VALUES (1, "0123456789abcdef", "http://localhost:3000/");
 
-INSERT INTO players(playerName)
-VALUES ("speed jesus");
-
-INSERT INTO scores(date, playerID, apiKey, score)
-VALUES('19891024 09:22:32 AM', 1, "0123456789abcdef", 987654321);
+INSERT INTO scores(date, playerName, apiKey, score)
+VALUES('19891024 09:22:32 AM', 'speedJesus', "0123456789abcdef", 987654321);
  
+-- http://localhost:3000/scores?api=sean
