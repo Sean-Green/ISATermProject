ccb84664d09ab181e72dd86b2fb39ce7b751820e
Sean <sean.joseph.green@gmail.com>
2021-03-30 09:40:47 -0700

    Added signup endpoint to app.js, had to adjust the schema as well

--- api/schema.sql
+++ api/schema.sql
@@ -1,35 +1,32 @@
 CREATE TABLE users(
-   userID      INT            NOT NULL AUTO_INCREMENT,
    email       VARCHAR(32)    NOT NULL, 
    password    VARCHAR(32)    NOT NULL,
-   PRIMARY KEY (userID)
+   PRIMARY KEY (email)
 );
 
--- Create DB
 CREATE TABLE apiKeys(
-   userID      INT            NOT NULL,
+   email       VARCHAR(32)    NOT NULL, 
    apiKey      CHAR(16)       NOT NULL,
    domain      VARCHAR(512)   NOT NULL,
    PRIMARY KEY (apiKey),
-   CONSTRAINT fk_userID FOREIGN KEY (userID) REFERENCES users(userID)
+   CONSTRAINT fk_email FOREIGN KEY (email) REFERENCES users(email)
 );
 
 CREATE TABLE scores(
-   scoreDate        DATETIME       NOT NULL,  -- YYYY-MM-DD HH:MI:SS PM
+   scoreID     INT            NOT NULL AUTO_INCREMENT,
+   scoreDate   DATETIME       NOT NULL,  -- YYYY-MM-DD HH:MI:SS PM
    playerName  VARCHAR(32)    NOT NULL,
    apiKey      CHAR(16)       NOT NULL,
    score       FLOAT          NOT NULL,
-   PRIMARY KEY (scoreDate, playerName, apiKey),
+   PRIMARY KEY (scoreID),
    CONSTRAINT fk_apiKey FOREIGN KEY (apiKey) REFERENCES apiKeys(apiKey)
 );
 
 INSERT INTO users(email, password)
 VALUES ("johnny@scottmail.ca", "ilovescatman");
 
-INSERT INTO apiKeys(userID, apiKey, domain)
-VALUES (1, "0123456789abcdef", "http://localhost:3000/");
+INSERT INTO apiKeys(email, apiKey, domain)
+VALUES ("johnny@scottmail.ca", "0123456789abcdef", "localhost");
 
 INSERT INTO scores(scoreDate, playerName, apiKey, score)
 VALUES('1989-10-24 09:22:32 AM', 'speedJesus', "0123456789abcdef", 987654321);
- 
--- http://localhost:3000/scores?api=sean
