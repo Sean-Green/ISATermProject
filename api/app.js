4afde109c1c5c4b0b21d4ac4104e1b9204b4fdac
Sean <sean.joseph.green@gmail.com>
2021-04-10 20:30:40 -0700

    work

--- api/app.js
+++ api/app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 3000;
 var clientOrigin;
 // clientOrigin = 'http://127.0.0.1:5500';
-// clientOrigin = 'https://www.johnnyscott.ca/'
+// clientOrigin = 'https://www.johnnyscott.ca'
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://www.johnnyscott.ca');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers',
         'Content-Type, Authorization, Content-Length, X-Requested-With');
